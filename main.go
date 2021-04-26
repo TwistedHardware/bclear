2bdbbbc0237b9aa8b1c14d3078f52dc58d774bfb
Abdullah Alrasheed <twistedhardware@gmail.com>
2021-04-26 14:07:44 +0400

    port 80

--- main.go
+++ main.go
@@ -1,22 +1,24 @@
 package main
 
 import (
-	"github.com/uadmin/uadmin"
 	"time"
+
+	"github.com/uadmin/uadmin"
 )
 
 type Task struct {
 	uadmin.Model
-	Name string `uadmin:"search"`
-	Description string `uadmin:"html;search"`
-	Deadline time.Time `uadmin:"filter"`
-	Progress float64 `uadmin:"progress_bar:40:red,70:yellow,100:lime"`
-	Attachment string `uadmin:"image"`
+	Name        string    `uadmin:"search"`
+	Description string    `uadmin:"html;search"`
+	Deadline    time.Time `uadmin:"filter"`
+	Progress    float64   `uadmin:"progress_bar:40:red,70:yellow,100:lime"`
+	Attachment  string    `uadmin:"image"`
 }
 
 func main() {
 	uadmin.Register(
 		Task{},
 	)
+	uadmin.Port = 80
 	uadmin.StartServer()
 }
